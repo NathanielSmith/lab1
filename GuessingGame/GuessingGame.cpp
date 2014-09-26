a52fd9b52ae0d362d42aebff8134de17e48bc452
NathanielSmith <smithnd13@uww.edu>
2014-09-26 13:09:46 -0500

    1.1
    Changed the number if guess to infinite

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -10,31 +10,35 @@
 #include <ctime>     // Used to generate a random seed for a RNG
 #include <cstdlib>  //  Used for the random function
 
-
-
 using namespace std;
 
 int main()
 {
 	int guess, key;
 	guess = key =- 1;
+	int cntr;
 
 	srand(time(NULL));
-
 	key = 10 + (rand() %90);
 
+	while(!(guess == key)){
+
+
+	cntr++;
 	cout << "Enter your guess: ";
 	cin >> guess;
 
-	if(guess == key){
-		cout <<"YOU WIN!!!" ;
+	if(guess > key){
+		cout << "Guess smaller numbers: \n"<< cntr << endl;
 	}
-	else{
-		cout<<"YOU FAIL!!!";
+	else if(guess < key){
+		cout << "Guess larger numbers: \n" << cntr << endl;
 	}
-
-
-
+	else {
+		cout << "You ARE A WINNER!!! \n:" << "Number of Guesses: " << guess<< endl;
+	}
+		
+	}
 
 }
 
